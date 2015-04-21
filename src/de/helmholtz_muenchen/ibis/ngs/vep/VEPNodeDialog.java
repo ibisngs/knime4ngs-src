c862f2bb9f10e2211830802de450bb0c3e85b2a1
timjeske <tim.jeske@yahoo.de>
2015-04-21 17:28:59 +0000

    option outfolder, transcript set, plugin ExAC, CADD implemented, NodeDescription reviewed

--- src/de/helmholtz_muenchen/ibis/ngs/vep/VEPNodeDialog.java
+++ src/de/helmholtz_muenchen/ibis/ngs/vep/VEPNodeDialog.java
@@ -2,9 +2,9 @@ package de.helmholtz_muenchen.ibis.ngs.vep;
 
 import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
 import org.knime.core.node.defaultnodesettings.DialogComponentBoolean;
-import org.knime.core.node.defaultnodesettings.DialogComponentButtonGroup;
 import org.knime.core.node.defaultnodesettings.DialogComponentFileChooser;
 import org.knime.core.node.defaultnodesettings.DialogComponentNumber;
+import org.knime.core.node.defaultnodesettings.DialogComponentStringSelection;
 import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
 import org.knime.core.node.defaultnodesettings.SettingsModelInteger;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
@@ -29,45 +29,69 @@ public class VEPNodeDialog extends HTExecutorNodeDialog {
      * New pane for configuring the VEP node.
      */
 	
-	//VEP tab
-	private final SettingsModelString vcfin = new SettingsModelString(VEPNodeModel.CFGKEY_VCF_INFILE,"-");
+	//VEP options tab
 	private final SettingsModelString veppl = new SettingsModelString(VEPNodeModel.CFGKEY_VEP_PL,"-");
-	private final SettingsModelString stats_type = new SettingsModelString(VEPNodeModel.CFGKEY_STATS_TYPE,"html");
-	private final SettingsModelBoolean coding_only = new SettingsModelBoolean(VEPNodeModel.CFGKEY_CODING_ONLY, true);
-	private final SettingsModelInteger forks = new SettingsModelInteger(VEPNodeModel.CFGKEY_FORKS,1);
+	private final SettingsModelString vcfin = new SettingsModelString(VEPNodeModel.CFGKEY_VCF_INFILE,"-");
+	private final SettingsModelString outfolder = new SettingsModelString(VEPNodeModel.CFGKEY_OUTFOLDER,"");
+	private final SettingsModelBoolean overwrite = new SettingsModelBoolean(VEPNodeModel.CFGKEY_OVERWRITE,false);
 	private final SettingsModelBoolean use_cache = new SettingsModelBoolean(VEPNodeModel.CFGKEY_USE_CACHE,true);
-	private final SettingsModelString cache_dir = new SettingsModelString(VEPNodeModel.CFGKEY_CACHE_DIR, VEPNodeModel.DEF_CACHE_DIR);
-	private final SettingsModelString plugin_dir = new SettingsModelString(VEPNodeModel.CFGKEY_PLUGIN_DIR, VEPNodeModel.DEF_CACHE_DIR);
+	private final SettingsModelInteger forks = new SettingsModelInteger(VEPNodeModel.CFGKEY_FORKS,1);
+	private final SettingsModelBoolean coding_only = new SettingsModelBoolean(VEPNodeModel.CFGKEY_CODING_ONLY, true);
+	private final SettingsModelString transcript_set = new SettingsModelString(VEPNodeModel.CFGKEY_TRANSCRIPT_SET,"Ensembl");
 
+	//advanced tab
+	private final SettingsModelString stats_type = new SettingsModelString(VEPNodeModel.CFGKEY_STATS_TYPE,"html");
+	private final SettingsModelString cache_dir = new SettingsModelString(VEPNodeModel.CFGKEY_CACHE_DIR, VEPNodeModel.DEF_CACHE_DIR);
+	private final SettingsModelString plugin_dir = new SettingsModelString(VEPNodeModel.CFGKEY_PLUGIN_DIR, VEPNodeModel.DEF_PLUGIN_DIR);
+	private final SettingsModelString tabix_path = new SettingsModelString(VEPNodeModel.CFGKEY_TABIX_PATH,"");
+	
 	//LOFTEE tab
 	private final SettingsModelBoolean use_loftee = new SettingsModelBoolean(VEPNodeModel.CFGKEY_USE_LOFTEE,false);
 	private final SettingsModelString human_ancestor = new SettingsModelString(VEPNodeModel.CFGKEY_HUMAN_ANCESTOR,"-");
 	private final SettingsModelString conservation_file = new SettingsModelString(VEPNodeModel.CFGKEY_CONSERVATION_FILE,"-");
 	private final SettingsModelString samtools_path = new SettingsModelString(VEPNodeModel.CFGKEY_SAMTOOLS_PATH, "-");
-
+	
+	//CADD tab
+	private final SettingsModelBoolean use_cadd = new SettingsModelBoolean(VEPNodeModel.CFGKEY_USE_CADD,false);
+	private final SettingsModelString first_cadd_file = new SettingsModelString(VEPNodeModel.CFGKEY_FIRST_CADD_FILE,"");
+	private final SettingsModelString sec_cadd_file = new SettingsModelString(VEPNodeModel.CFGKEY_SEC_CADD_FILE,"");
+	
+	//ExAC tab
+	private final SettingsModelBoolean use_exac = new SettingsModelBoolean(VEPNodeModel.CFGKEY_USE_EXAC,false);
+	private final SettingsModelString exac_file = new SettingsModelString(VEPNodeModel.CFGKEY_EXAC_FILE,"");
 	
     protected VEPNodeDialog() {
 
     	super();
     	
-    	createNewGroup("Path to VCF file");
-    	addDialogComponent(new DialogComponentFileChooser(vcfin, "his_id_VEP_VCFIN", 0, ".vcf"));
-    	
     	createNewGroup("Path to variant_effect_predictor.pl");
     	addDialogComponent(new DialogComponentFileChooser(veppl, "his_id_VEP_VEPPL", 0, ".pl"));
     	
-    	createNewGroup("Cache usage");
-    	addDialogComponent(new DialogComponentBoolean(use_cache, "Use cache?"));
-    	addDialogComponent(new DialogComponentFileChooser(cache_dir, "his_id_VEP_CACHEDIR", 0, true));
+    	createNewGroup("Path to VCF file");
+    	addDialogComponent(new DialogComponentFileChooser(vcfin, "his_id_VEP_VCFIN", 0, ".vcf"));
+    	
+    	createNewGroup("Folder for output files");
+    	addDialogComponent(new DialogComponentFileChooser(outfolder, "his_id_VEP_OUT", 0, true));
+    	addDialogComponent(new DialogComponentBoolean(overwrite, "Overwrite, if output files exist?"));
     	
     	createNewGroup("Further options");
-    	addDialogComponent(new DialogComponentBoolean(coding_only, "Coding only?"));
-    	addDialogComponent(new DialogComponentButtonGroup(stats_type, false, "Choose type of statistic file", VEPNodeModel.STAT_TYPES));
+    	addDialogComponent(new DialogComponentBoolean(use_cache, "Use cache?"));
     	addDialogComponent(new DialogComponentNumber(forks, "Number of forks",1));
+    	addDialogComponent(new DialogComponentBoolean(coding_only, "Coding only?"));
+    	addDialogComponent(new DialogComponentStringSelection(transcript_set,"Choose transcript set", VEPNodeModel.TRANSCRIPT_SETS));
+
+    	createNewTab("Advanced");
+    	addDialogComponent(new DialogComponentStringSelection(stats_type,"Choose type of statistic file", VEPNodeModel.STAT_TYPES));
+    	
+    	createNewGroup("Cache directory");
+    	addDialogComponent(new DialogComponentFileChooser(cache_dir, "his_id_VEP_CACHEDIR", 0, true));
     	
     	createNewGroup("Plugin directory");
     	addDialogComponent(new DialogComponentFileChooser(plugin_dir, "his_id_VEP_PLUGINDIR", 0, true));
-
+    	
+    	createNewGroup("Tabix PATH (required by CADD and ExAC");
+    	addDialogComponent(new DialogComponentFileChooser(tabix_path, "his_id_TABIX_PATH",0,true));
+    	
     	createNewTab("LOFTEE");
     	addDialogComponent(new DialogComponentBoolean(use_loftee, "Use LOFTEE?"));
     	
@@ -79,6 +103,21 @@ public class VEPNodeDialog extends HTExecutorNodeDialog {
     	
     	createNewGroup("Samtools PATH");
     	addDialogComponent(new DialogComponentFileChooser(samtools_path, "his_id_VEP_SAMTOOLSPATH", 0, true));
+    	
+    	createNewTab("CADD");
+    	addDialogComponent(new DialogComponentBoolean(use_cadd,"Use CADD?"));
+    	
+    	createNewGroup("Path to a CADD file");
+    	addDialogComponent(new DialogComponentFileChooser(first_cadd_file,"his_id_FIRST_CADD_FILE",0,".tsv.gz"));
+    	
+    	createNewGroup("Path to another CADD file");
+    	addDialogComponent(new DialogComponentFileChooser(sec_cadd_file,"his_id_SEC_CADD_FILE",0,".tsv.gz"));
+    	
+    	createNewTab("ExAC");
+    	addDialogComponent(new DialogComponentBoolean(use_exac,"Use ExAC?"));
+    	
+    	createNewGroup("Path to ExAC file");
+    	addDialogComponent(new DialogComponentFileChooser(exac_file, "his_id_EXAC_FILE",0, ".vcf.gz"));
     }
 }
 
--- src/de/helmholtz_muenchen/ibis/ngs/vep/VEPNodeModel.java
+++ src/de/helmholtz_muenchen/ibis/ngs/vep/VEPNodeModel.java
@@ -2,6 +2,7 @@ package de.helmholtz_muenchen.ibis.ngs.vep;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.knime.core.data.DataColumnSpec;
 import org.knime.core.data.DataColumnSpecCreator;
@@ -16,6 +17,7 @@ import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeLogger;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.defaultnodesettings.SettingsModel;
 import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
 import org.knime.core.node.defaultnodesettings.SettingsModelInteger;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
@@ -38,33 +40,46 @@ public class VEPNodeModel extends HTExecutorNodeModel {
 	// the logger instance
     protected static final NodeLogger logger = NodeLogger.getLogger(VATNodeModel.class);
     
-	static final String CFGKEY_VCF_INFILE = "vcf_infile";
-	final SettingsModelString m_vcfin = new SettingsModelString(CFGKEY_VCF_INFILE,"");
-	
-	static final String CFGKEY_VEP_PL = "vep_pl";
+    ArrayList<SettingsModel> models = new ArrayList<SettingsModel>();
+    
+    //general options
+    static final String CFGKEY_VEP_PL = "vep_pl";
 	final SettingsModelString m_veppl = new SettingsModelString(CFGKEY_VEP_PL,"");
 	
-	static final String CFGKEY_STATS_TYPE = "stats_type";
-	static final String [] STAT_TYPES = {"none", "html", "plain text"};
-	final SettingsModelString m_stats_type = new SettingsModelString(CFGKEY_STATS_TYPE,"");
+    static final String CFGKEY_VCF_INFILE = "vcf_infile";
+	final SettingsModelString m_vcfin = new SettingsModelString(CFGKEY_VCF_INFILE,"");
 	
-	static final String CFGKEY_CODING_ONLY = "coding_only";
-	final SettingsModelBoolean m_coding_only = new SettingsModelBoolean(CFGKEY_CODING_ONLY,true);
+	static final String CFGKEY_OUTFOLDER = "outfolder";
+	final SettingsModelString m_outfolder = new SettingsModelString(CFGKEY_OUTFOLDER,"");
+	
+	static final String CFGKEY_OVERWRITE = "overwrite";
+	private final SettingsModelBoolean m_overwrite = new SettingsModelBoolean(CFGKEY_OVERWRITE, false);
+	
+	static final String CFGKEY_USE_CACHE = "use_cache";
+	final SettingsModelBoolean m_use_cache = new SettingsModelBoolean(CFGKEY_USE_CACHE,true);
 	
 	static final String CFGKEY_FORKS = "forks";
 	private final SettingsModelInteger m_forks = new SettingsModelInteger(CFGKEY_FORKS,1);
 	
-	//cache usage
-	static final String CFGKEY_USE_CACHE = "use_cache";
-	final SettingsModelBoolean m_use_cache = new SettingsModelBoolean(CFGKEY_USE_CACHE,true);
+	static final String CFGKEY_CODING_ONLY = "coding_only";
+	final SettingsModelBoolean m_coding_only = new SettingsModelBoolean(CFGKEY_CODING_ONLY,true);
+	
+	static final String CFGKEY_TRANSCRIPT_SET ="transcript_set";
+	static final String [] TRANSCRIPT_SETS = {"Ensembl","RefSeq","GENCODE Basic"};
+	final SettingsModelString m_transcript_set = new SettingsModelString(CFGKEY_TRANSCRIPT_SET,"");
+	
+	//advanced options: cache directory, plugin directory, stats type
+	static final String CFGKEY_STATS_TYPE = "stats_type";
+	static final String [] STAT_TYPES = {"none", "html", "plain text"};
+	final SettingsModelString m_stats_type = new SettingsModelString(CFGKEY_STATS_TYPE,"");
 	
 	static final String CFGKEY_CACHE_DIR = "cache_dir";
 	static final String DEF_CACHE_DIR = System.getProperty("user.home") + System.getProperty("file.separator") + ".vep";
 	final SettingsModelString m_cache_dir = new SettingsModelString(CFGKEY_CACHE_DIR, DEF_CACHE_DIR);
 	
-	//pugin directory
 	static final String CFGKEY_PLUGIN_DIR = "plugin_dir";
-	final SettingsModelString m_plugin_dir = new SettingsModelString(CFGKEY_PLUGIN_DIR, DEF_CACHE_DIR);
+	static final String DEF_PLUGIN_DIR = DEF_CACHE_DIR + System.getProperty("file.separator") + "Plugins";
+	final SettingsModelString m_plugin_dir = new SettingsModelString(CFGKEY_PLUGIN_DIR, DEF_PLUGIN_DIR);
 	
 	//loftee
 	static final String CFGKEY_USE_LOFTEE = "use_loftee";
@@ -79,6 +94,28 @@ public class VEPNodeModel extends HTExecutorNodeModel {
 	static final String CFGKEY_SAMTOOLS_PATH = "samtools_path";
 	final SettingsModelString m_samtools_path = new SettingsModelString(CFGKEY_SAMTOOLS_PATH,"");
 	
+	
+	//tabix path
+	static final String CFGKEY_TABIX_PATH = "tabix_path";
+	final SettingsModelString m_tabix_path = new SettingsModelString(CFGKEY_TABIX_PATH,"");
+	
+	//CADD
+	static final String CFGKEY_USE_CADD = "use_cadd";
+	final SettingsModelBoolean m_use_cadd = new SettingsModelBoolean(CFGKEY_USE_CADD,false);
+	
+	static final String CFGKEY_FIRST_CADD_FILE = "first_cadd_file";
+	final SettingsModelString m_first_cadd_file = new SettingsModelString(CFGKEY_FIRST_CADD_FILE,"");
+	
+	static final String CFGKEY_SEC_CADD_FILE = "sec_cadd_file";
+	final SettingsModelString m_sec_cadd_file = new SettingsModelString(CFGKEY_SEC_CADD_FILE,"");
+	
+	//ExAC
+	static final String CFGKEY_USE_EXAC = "use_exac";
+	final SettingsModelBoolean m_use_exac = new SettingsModelBoolean(CFGKEY_USE_EXAC,false);
+	
+	static final String CFGKEY_EXAC_FILE = "exac_file";
+	final SettingsModelString m_exac_file = new SettingsModelString(CFGKEY_EXAC_FILE,"");
+	
 	public static final String OUT_COL1 = "Path2VEP_AnnotationVCF";
 	public static final String OUT_COL2 = "Path2VEP_StatsFile";
 	
@@ -88,7 +125,6 @@ public class VEPNodeModel extends HTExecutorNodeModel {
      * Constructor for the node model.
      */
     protected VEPNodeModel() {
-    
     	super(OptionalPorts.createOPOs(1, 1), OptionalPorts.createOPOs(1));
     }
 
@@ -98,42 +134,55 @@ public class VEPNodeModel extends HTExecutorNodeModel {
     @Override
     protected BufferedDataTable[] execute(final BufferedDataTable[] inData,
             final ExecutionContext exec) throws Exception {
-
-    	String vcf_infile, res_file, stats_file, stdOutFile, stdErrFile, cmd;
-    	String environment = "";
-    	File lockFile;
-//    	String fileBase;
+    	
+    	String cmd;
+    	
     	
     	//perl script
-    	cmd = "perl "+m_veppl.getStringValue();
+    	String script = m_veppl.getStringValue();
+    	if(script.equals("")) {
+    		System.err.println("Path to variant_effect_predictor.pl was not specified!");
+    	}
+    	cmd = "perl "+script;
     	
     	//input file
+    	String vcf_infile;
     	if(optionalPort){	//Input Table available
     		//Get File via Table
     		vcf_infile = inData[0].iterator().next().getCell(0).toString();
     	}else{
     		//Get File via FileSelector
     		vcf_infile = m_vcfin.getStringValue();
+    		if(vcf_infile.equals("")) {
+    			System.err.println("No input vcf file specified!");
+    		}
     	}
     	cmd += " -i " + vcf_infile;
-//    	fileBase = PathProcessor.getBase(vcf_infile);
+    	
+    	String outfileBase = m_outfolder.getStringValue()+ System.getProperty("file.separator")+ new File(vcf_infile).getName();
     	
     	//output file + stats file
-    	res_file = vcf_infile.replace("vcf", "VEP_annotation.vcf");
-//    	res_file = PathProcessor.createOutputFile(fileBase,"vcf","VEP_annotation");
+    	String res_file,stats_file,stats_type;
+    	res_file = outfileBase.replace("vcf", "vep.vcf");
     	cmd += " -o " + res_file;
     		
-    	if(m_stats_type.getStringValue().equals("html")) {
-    		stats_file = vcf_infile.replace("vcf", "VEP_stats.html");
-//    		stats_file = PathProcessor.createOutputFile(fileBase, "html", "VEP_stats");
+    	stats_type = m_stats_type.getStringValue();
+    	if(stats_type.equals("html")) {
+    		stats_file = outfileBase.replace("vcf", "vep_stats.html");
     		cmd += " --stats_file " + stats_file;
-    	} else if(m_stats_type.getStringValue().equals("plain text")) {
-    		stats_file = vcf_infile.replace("vcf", "VEP_stats.txt");
-//    		stats_file = PathProcessor.createOutputFile(fileBase, "txt", "VEP_stats");
+    	} else if(stats_type.equals("plain text")) {
+    		stats_file = outfileBase.replace("vcf", "vep_stats.txt");
     		cmd += " --stats_text " + stats_file;
     	} else {
     		stats_file = "";
     		cmd += " --no_stats";
+    	}
+    	
+    	String transcriptSet= m_transcript_set.getStringValue();
+    	if(transcriptSet.equals("RefSeq")) {
+    		cmd += " --refseq";
+    	} else if(transcriptSet.equals("GENCODE Basic")) {
+    		cmd += " --gencode_basic";
     	}
     	
     	if(m_coding_only.getBooleanValue()) {
@@ -144,9 +193,17 @@ public class VEPNodeModel extends HTExecutorNodeModel {
     		cmd += " --fork " + m_forks.getIntValue();
     	}
     	
+    	if(m_overwrite.getBooleanValue()) {
+    		cmd += " --force_overwrite";
+    	}
+    	
     	//cache usage
+    	String cache_dir = m_cache_dir.getStringValue();
     	if(m_use_cache.getBooleanValue()) {
-    		cmd += " --dir_cache " + m_cache_dir.getStringValue(); 
+    		if(cache_dir.equals("")) {
+    			System.err.println("Cache directory was not specified!");
+    		}
+    		cmd += " --dir_cache " + cache_dir; 
     		cmd += " --offline";
     	} else {
     		cmd += " --database";
@@ -155,24 +212,76 @@ public class VEPNodeModel extends HTExecutorNodeModel {
     	//default VEP parameters
     	cmd += " --no_progress";
     	cmd += " --vcf";
+    	cmd += " --pubmed";
+    	
+    	
+    	//plugin parameters
+    	String plugin_dir = m_plugin_dir.getStringValue();
+    	if(plugin_dir.equals("")) {
+    		plugin_dir = System.getProperty("file.separator");
+    		System.err.println("Plugin directory was not specified!");
+    	}
+    	cmd += " --dir_plugins " + plugin_dir;
+    	
+    	String environment;
+    	String samtools_path = m_samtools_path.getStringValue();
+    	if(samtools_path.equals("")) {
+    		System.err.println("Samtools PATH was not specified!");
+    	}
+    	String tabix_path = m_tabix_path.getStringValue();
+    	if(tabix_path.equals("")) {
+    		System.err.println("Tabix path was not specified!");
+    	}
+    	environment = "PATH="+System.getenv("PATH")+":"+samtools_path;
+    	environment += ":"+tabix_path;
     	
     	//LOFTEE parameters
     	if(m_use_loftee.getBooleanValue()) {
-    		cmd += " --dir_plugins " + m_plugin_dir.getStringValue();
     		cmd += " --plugin LoF";
-    		cmd += ",human_ancestor_fa:"+m_human_ancestor.getStringValue();
-    		if(m_forks.getIntValue()<=1) {
-    			cmd += ",conservation_file:"+m_conservation_file.getStringValue();
-    		}
     		
-    		environment = "PATH="+System.getenv("PATH")+":"+m_samtools_path.getStringValue();
+    		String human_ancestor = m_human_ancestor.getStringValue();
+    		if(human_ancestor.equals("")) {
+    			System.err.println("Human ancestor file was not specified!");
+    		}
+    		cmd += ",human_ancestor_fa:"+human_ancestor;
+    		if(m_forks.getIntValue()<=1) {
+    			String conservation_file = m_conservation_file.getStringValue();
+    			if(conservation_file.equals("")) {
+    				System.err.println("Phylocsf.sql was not specified!");
+    			}
+    			cmd += ",conservation_file:"+conservation_file;
+    		}
     	}
     	
-    	stdOutFile = vcf_infile.replace("vcf","VEP_stdout.vcf");
-//    	stdOutFile = PathProcessor.createOutputFile(fileBase, "txt", "VEP_stdout");
-    	stdErrFile = vcf_infile.replace("vcf","VEP_stderr.vcf");
-//    	stdErrFile = PathProcessor.createOutputFile(fileBase, "txt", "VEP_stderr");
-    	lockFile = new File(vcf_infile.substring(0,vcf_infile.lastIndexOf(".")) + ".VEP" +  SuccessfulRunChecker.LOCK_ENDING);
+    	//CADD parameters
+    	String first_cadd_file = m_first_cadd_file.getStringValue();
+    	String sec_cadd_file = m_sec_cadd_file.getStringValue();
+    	if(first_cadd_file.equals("") && sec_cadd_file.equals("")) {
+    		System.err.println("No CADD file specified!");
+    	}
+    	if(m_use_cadd.getBooleanValue()) {
+    		cmd += " --plugin CADD";
+    		if(!first_cadd_file.equals("")) {
+    			cmd += ","+first_cadd_file;
+    		}
+    		if(!sec_cadd_file.equals("")) {
+    			cmd += ","+sec_cadd_file;
+    		}
+    	}
+    	
+    	//ExAC parameters
+    	String exac_file = m_exac_file.getStringValue();
+    	if(exac_file.equals("")) {
+			System.err.println("No ExAC file specified!");
+		}
+    	if(m_use_exac.getBooleanValue()) {
+    		cmd += " --plugin ExAC";
+    		cmd += ","+exac_file;
+    	}
+    	
+    	String stdOutFile = outfileBase.replace("vcf", "vep.stdout");
+    	String stdErrFile = outfileBase.replace("vcf", "vep.stderr");
+    	File lockFile = new File(outfileBase.replace("vcf", "vep" + SuccessfulRunChecker.LOCK_ENDING));
 
     	System.out.println("COMMAND: "+cmd);
     	System.out.println("ENVIRONMENT: "+environment);
@@ -231,6 +340,7 @@ public class VEPNodeModel extends HTExecutorNodeModel {
     	super.saveSettingsTo(settings);
         m_veppl.saveSettingsTo(settings);
         m_vcfin.saveSettingsTo(settings);
+        m_outfolder.saveSettingsTo(settings);
         m_stats_type.saveSettingsTo(settings);
         m_coding_only.saveSettingsTo(settings);
         m_use_cache.saveSettingsTo(settings);
@@ -241,6 +351,14 @@ public class VEPNodeModel extends HTExecutorNodeModel {
         m_conservation_file.saveSettingsTo(settings);
         m_samtools_path.saveSettingsTo(settings);
         m_forks.saveSettingsTo(settings);
+        m_transcript_set.saveSettingsTo(settings);
+        m_overwrite.saveSettingsTo(settings);
+        m_tabix_path.saveSettingsTo(settings);
+        m_use_cadd.saveSettingsTo(settings);
+        m_first_cadd_file.saveSettingsTo(settings);
+        m_sec_cadd_file.saveSettingsTo(settings);
+        m_use_exac.saveSettingsTo(settings);
+        m_exac_file.saveSettingsTo(settings);
     }
 
     /**
@@ -252,6 +370,7 @@ public class VEPNodeModel extends HTExecutorNodeModel {
     	super.loadValidatedSettingsFrom(settings);
         m_veppl.loadSettingsFrom(settings);
         m_vcfin.loadSettingsFrom(settings);
+        m_outfolder.loadSettingsFrom(settings);
         m_stats_type.loadSettingsFrom(settings);
         m_coding_only.loadSettingsFrom(settings);
         m_use_cache.loadSettingsFrom(settings);
@@ -262,6 +381,14 @@ public class VEPNodeModel extends HTExecutorNodeModel {
         m_conservation_file.loadSettingsFrom(settings);
         m_samtools_path.loadSettingsFrom(settings);
         m_forks.loadSettingsFrom(settings);
+        m_transcript_set.loadSettingsFrom(settings);
+        m_overwrite.loadSettingsFrom(settings);
+        m_tabix_path.loadSettingsFrom(settings);
+        m_use_cadd.loadSettingsFrom(settings);
+        m_first_cadd_file.loadSettingsFrom(settings);
+        m_sec_cadd_file.loadSettingsFrom(settings);
+        m_use_exac.loadSettingsFrom(settings);
+        m_exac_file.loadSettingsFrom(settings);
     }
 
     /**
@@ -273,6 +400,7 @@ public class VEPNodeModel extends HTExecutorNodeModel {
     	super.validateSettings(settings);
         m_veppl.validateSettings(settings);
         m_vcfin.validateSettings(settings);
+        m_outfolder.validateSettings(settings);
         m_stats_type.validateSettings(settings);
         m_coding_only.validateSettings(settings);
         m_use_cache.validateSettings(settings);
@@ -283,6 +411,14 @@ public class VEPNodeModel extends HTExecutorNodeModel {
         m_conservation_file.validateSettings(settings);
         m_samtools_path.validateSettings(settings);
         m_forks.validateSettings(settings);
+        m_transcript_set.validateSettings(settings);
+        m_overwrite.validateSettings(settings);
+        m_tabix_path.validateSettings(settings);
+        m_use_cadd.validateSettings(settings);
+        m_first_cadd_file.validateSettings(settings);
+        m_sec_cadd_file.validateSettings(settings);
+        m_use_exac.validateSettings(settings);
+        m_exac_file.validateSettings(settings);
     }
     
     /**
