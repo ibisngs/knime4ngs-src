31c10edc1b72de3e3c4e85437a848480ee87cc1f
timjeske <tim.jeske@yahoo.de>
2015-09-17 13:26:22 +0000

    HaplotypeCaller checked

---
+++ src/de/helmholtz_muenchen/ibis/ngs/gatkhaplotypecaller/GATKHaplotypeCallerNodeDialog.java
@@ -0,0 +1,29 @@
+package de.helmholtz_muenchen.ibis.ngs.gatkhaplotypecaller;
+
+
+import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+
+import de.helmholtz_muenchen.ibis.utils.abstractNodes.GATKNode.GATKNodeDialog;
+
+/**
+ * <code>NodeDialog</code> for the "GATKHaplotypeCaller" Node.
+ * 
+ *
+ * This node dialog derives from {@link DefaultNodeSettingsPane} which allows
+ * creation of a simple dialog with standard components. If you need a more 
+ * complex dialog please derive directly from 
+ * {@link org.knime.core.node.NodeDialogPane}.
+ * 
+ * @author Maximilian Hastreiter
+ */
+public class GATKHaplotypeCallerNodeDialog extends GATKNodeDialog {
+
+	@Override
+	protected void addDialogComponent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
+
