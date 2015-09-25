8fc0497308b30387ad5ca3c4af212d0042d115e5
timjeske <tim.jeske@yahoo.de>
2015-09-25 09:43:08 +0000

    abstract node SelectVariants extends GATKNode

--- src/de/helmholtz_muenchen/ibis/utils/abstractNodes/SelectVariants/SelectVariantsNodeDialog.java
+++ src/de/helmholtz_muenchen/ibis/utils/abstractNodes/SelectVariants/SelectVariantsNodeDialog.java
@@ -1,12 +1,12 @@
 package de.helmholtz_muenchen.ibis.utils.abstractNodes.SelectVariants;
 
-import javax.swing.JFileChooser;
 
 import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
-import org.knime.core.node.defaultnodesettings.DialogComponentFileChooser;
 import org.knime.core.node.defaultnodesettings.DialogComponentNumber;
 import org.knime.core.node.defaultnodesettings.SettingsModelIntegerBounded;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
+
+import de.helmholtz_muenchen.ibis.utils.abstractNodes.GATKNode.GATKNodeDialog;
 
 /**
  * <code>NodeDialog</code> for the "GATKSelectVariants" Node.
@@ -19,41 +19,47 @@ import org.knime.core.node.defaultnodesettings.SettingsModelString;
  * 
  * @author 
  */
-public abstract class SelectVariantsNodeDialog extends DefaultNodeSettingsPane {
+public abstract class SelectVariantsNodeDialog extends GATKNodeDialog {
 
 	/**
 	 * The SettingsModels
 	 */
 	
-    private final SettingsModelString GATK = new SettingsModelString(SelectVariantsNodeModel.CFGKEY_GATK_PATH, "");
-    private final SettingsModelString REF_GENOME = new SettingsModelString(SelectVariantsNodeModel.CFGKEY_REF_GENOME, "");
-    private final SettingsModelIntegerBounded VCFCOLUMN = new SettingsModelIntegerBounded(SelectVariantsNodeModel.CFGKEY_VCFCOLUMN, 1, 1, Integer.MAX_VALUE);
-    private final SettingsModelString m_FILTERSTRING = new SettingsModelString(SelectVariantsNodeModel.CFGKEY_FILTERSTRING, "");
+//    private final SettingsModelString m_FILTERSTRING; 
     
     /**
      * New pane for configuring the GATKSelectVariants node.
      */
-    protected SelectVariantsNodeDialog() {
-
-    	createNewGroup("Path to GATK jar file");
-    	DialogComponentFileChooser gatkf= new DialogComponentFileChooser(GATK, "gatk", JFileChooser.OPEN_DIALOG, false, ".jar");
-    	gatkf.setBorderTitle("Choose File (disabled if file available from previous node)");
-    	addDialogComponent(gatkf);
+//    protected SelectVariantsNodeDialog() {
+//
+//    	createNewGroup("Path to GATK jar file");
+//    	DialogComponentFileChooser gatkf= new DialogComponentFileChooser(GATK, "gatk", JFileChooser.OPEN_DIALOG, false, ".jar");
+//    	gatkf.setBorderTitle("Choose File (disabled if file available from previous node)");
+//    	addDialogComponent(gatkf);
+//    	
+//    	createNewGroup("Reference Genome");
+//    	DialogComponentFileChooser ref_genome= new DialogComponentFileChooser(REF_GENOME, "ref_genome_variant_filter", JFileChooser.OPEN_DIALOG, false, ".txt|.fa|.fasta");
+//    	gatkf.setBorderTitle("Choose the reference genome");
+//    	addDialogComponent(ref_genome);
+//    	
+//        createNewGroup("");
+//        addDialogComponent(new DialogComponentNumber(VCFCOLUMN, "Select Column of inData in which Path to Input VCF is specified", 1));
+//    	
+//        addFilterDialogComponent();
+//        
+//    }
+    
+    protected void addDialogComponent() {
     	
-    	createNewGroup("Reference Genome");
-    	DialogComponentFileChooser ref_genome= new DialogComponentFileChooser(REF_GENOME, "ref_genome_variant_filter", JFileChooser.OPEN_DIALOG, false, ".txt|.fa|.fasta");
-    	gatkf.setBorderTitle("Choose the reference genome");
-    	addDialogComponent(ref_genome);
-    	
+        SettingsModelIntegerBounded VCFCOLUMN = new SettingsModelIntegerBounded(SelectVariantsNodeModel.CFGKEY_VCFCOLUMN, 1, 1, Integer.MAX_VALUE);
+    	createNewTab("SelectVariants");
         createNewGroup("");
-        addDialogComponent(new DialogComponentNumber(VCFCOLUMN, "Select Column of inData in which Path to Input VCF is specified", 1));
-    	
+        addDialogComponent(new DialogComponentNumber(VCFCOLUMN, "Column of input VCF file", 1));
         addFilterDialogComponent();
-        
     }
     
     protected SettingsModelString getFILTERSTRINGModel(){
-    	return m_FILTERSTRING;
+    	return new SettingsModelString(SelectVariantsNodeModel.CFGKEY_FILTERSTRING, "");
     }
     
     /**
--- src/de/helmholtz_muenchen/ibis/utils/abstractNodes/SelectVariants/SelectVariantsNodeModel.java
+++ src/de/helmholtz_muenchen/ibis/utils/abstractNodes/SelectVariants/SelectVariantsNodeModel.java
@@ -1,30 +1,20 @@
 package de.helmholtz_muenchen.ibis.utils.abstractNodes.SelectVariants;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 
 import org.apache.commons.lang3.StringUtils;
-import org.knime.core.data.DataColumnSpec;
-import org.knime.core.data.DataColumnSpecCreator;
-import org.knime.core.data.DataTableSpec;
-import org.knime.core.data.def.DefaultRow;
-import org.knime.core.node.BufferedDataContainer;
 import org.knime.core.node.BufferedDataTable;
-import org.knime.core.node.CanceledExecutionException;
-import org.knime.core.node.ExecutionContext;
-import org.knime.core.node.ExecutionMonitor;
 import org.knime.core.node.InvalidSettingsException;
-import org.knime.core.node.NodeLogger;
-import org.knime.core.node.NodeModel;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
 import org.knime.core.node.defaultnodesettings.SettingsModelIntegerBounded;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
 
-import de.helmholtz_muenchen.ibis.utils.datatypes.file.FileCell;
-import de.helmholtz_muenchen.ibis.utils.datatypes.file.FileCellFactory;
-import de.helmholtz_muenchen.ibis.utils.threads.Executor;
+import de.helmholtz_muenchen.ibis.utils.IO;
+import de.helmholtz_muenchen.ibis.utils.SuccessfulRunChecker;
+import de.helmholtz_muenchen.ibis.utils.abstractNodes.GATKNode.GATKNodeModel;
+import de.helmholtz_muenchen.ibis.utils.ngs.OptionalPorts;
 
 /**
  * This is the model implementation of GATKSelectVariants.
@@ -32,13 +22,11 @@ import de.helmholtz_muenchen.ibis.utils.threads.Executor;
  *
  * @author Maximilian Hastreiter
  */
-public abstract class SelectVariantsNodeModel extends NodeModel {
+public abstract class SelectVariantsNodeModel extends GATKNodeModel {
     
 	/**
 	 * Config Keys
 	 */
-	public static final String CFGKEY_GATK_PATH = "GATK_PATH";
-	public static final String CFGKEY_REF_GENOME = "REFGENOME";
 	public static final String CFGKEY_VCFCOLUMN = "VCFCOLUMN";
 	public static final String CFGKEY_FILTERSTRING = "FILTERSTRING";
 	
@@ -46,16 +34,11 @@ public abstract class SelectVariantsNodeModel extends NodeModel {
 	 * The SettingsModels
 	 */
 	
-    private final SettingsModelString m_GATK = new SettingsModelString(CFGKEY_GATK_PATH, "");
-    private final SettingsModelString m_REF_GENOME = new SettingsModelString(CFGKEY_REF_GENOME, "");
     private final SettingsModelIntegerBounded m_VCFCOLUMN = new SettingsModelIntegerBounded(CFGKEY_VCFCOLUMN, 1, 1, Integer.MAX_VALUE);
     private final SettingsModelString m_FILTERSTRING = new SettingsModelString(CFGKEY_FILTERSTRING, "");
-
-	/**
-	 * Logger
-	 */
-	private static final NodeLogger LOGGER = NodeLogger.getLogger(SelectVariantsNodeModel.class);
     
+	private String OUTFILE, LOCKFILE;
+	
 	//The Output Col Names
 	public static final String OUT_COL1_TABLE1 = "FilteredVCF";
     
@@ -63,148 +46,121 @@ public abstract class SelectVariantsNodeModel extends NodeModel {
      * Constructor for the node model.
      */
     protected SelectVariantsNodeModel(int INPORTS, int OUTPORTS) {
-        super(INPORTS, OUTPORTS);
+        super(OptionalPorts.createOPOs(1), OptionalPorts.createOPOs(1));
     }
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected BufferedDataTable[] execute(final BufferedDataTable[] inData,
-            final ExecutionContext exec) throws Exception {
 
-    	/**
-    	 * Get Input Data
-    	 */
+    protected String getCommandParameters(final BufferedDataTable[] inData) throws InvalidSettingsException {
+    	ArrayList<String> command = new ArrayList<String>();
+    	
     	String INVCF = inData[0].iterator().next().getCell(m_VCFCOLUMN.getIntValue()-1).toString();
     	if(!INVCF.contains(".vcf")){
     		throw new InvalidSettingsException("Infile "+INVCF+" seems to be in the wrong format. VCF File required!");
     	}
-    	
-    	ArrayList<String> command = new ArrayList<String>();
-    	
-    	command.add("java");
-    	command.add("-jar "+m_GATK.getStringValue());
-    	command.add("-T SelectVariants");
-    	
-    	command.add("-R "+m_REF_GENOME.getStringValue());
     	command.add("-V "+INVCF);
-    	
     	command.add(getCommandParameters());
+    	this.OUTFILE = IO.replaceFileExtension(INVCF, getOutfileSuffix());
+		this.LOCKFILE = IO.replaceFileExtension(INVCF, SuccessfulRunChecker.LOCK_ENDING);
     	
-    	String OUTFILE = INVCF.replace(".vcf", getOutfileSuffix());
-    	
-    	command.add("-o "+OUTFILE);
-    	
-    	System.out.println(StringUtils.join(command, " "));
-     	Executor.executeCommand(new String[]{StringUtils.join(command, " ")},exec,LOGGER);
-     	
-    	
-     	
-    	/**
-    	 * OUTPUT
-    	 */
-     	//Table1
-    	BufferedDataContainer cont = exec.createDataContainer(
-    			new DataTableSpec(
-    			new DataColumnSpec[]{
-    					new DataColumnSpecCreator(OUT_COL1_TABLE1, FileCell.TYPE).createSpec()}));
-    	
-    	FileCell[] c = new FileCell[]{
-    			(FileCell) FileCellFactory.create(OUTFILE)};
-    	
-    	cont.addRowToTable(new DefaultRow("Row0",c));
-    	cont.close();
-    	BufferedDataTable outTable1 = cont.getTable();
-
-
-        return new BufferedDataTable[]{outTable1};
- 
+    	return StringUtils.join(command, " ");
     }
-
+    
+    protected File getLockFile() {
+    	return new File(this.LOCKFILE);
+    }
     
     
+    protected String getOutfile() {
+    	return this.OUTFILE;
+    }
+
+//    protected BufferedDataTable[] execute(final BufferedDataTable[] inData,
+//            final ExecutionContext exec) throws Exception {
+//
+//    	/**
+//    	 * Get Input Data
+//    	 */
+//    	String INVCF = inData[0].iterator().next().getCell(m_VCFCOLUMN.getIntValue()-1).toString();
+//    	if(!INVCF.contains(".vcf")){
+//    		throw new InvalidSettingsException("Infile "+INVCF+" seems to be in the wrong format. VCF File required!");
+//    	}
+//    	
+//    	ArrayList<String> command = new ArrayList<String>();
+//    	
+//    	command.add("java");
+//    	command.add("-jar "+m_GATK.getStringValue());
+//    	command.add("-T SelectVariants");
+//    	
+//    	command.add("-R "+m_REF_GENOME.getStringValue());
+//    	command.add("-V "+INVCF);
+//    	
+//    	command.add(getCommandParameters());
+//    	
+//    	String OUTFILE = INVCF.replace(".vcf", getOutfileSuffix());
+//    	
+//    	command.add("-o "+OUTFILE);
+//    	
+//    	System.out.println(StringUtils.join(command, " "));
+//     	Executor.executeCommand(new String[]{StringUtils.join(command, " ")},exec,LOGGER);
+//     	
+//    	
+//     	
+//    	/**
+//    	 * OUTPUT
+//    	 */
+//     	//Table1
+//    	BufferedDataContainer cont = exec.createDataContainer(
+//    			new DataTableSpec(
+//    			new DataColumnSpec[]{
+//    					new DataColumnSpecCreator(OUT_COL1_TABLE1, FileCell.TYPE).createSpec()}));
+//    	
+//    	FileCell[] c = new FileCell[]{
+//    			(FileCell) FileCellFactory.create(OUTFILE)};
+//    	
+//    	cont.addRowToTable(new DefaultRow("Row0",c));
+//    	cont.close();
+//    	BufferedDataTable outTable1 = cont.getTable();
+//
+//
+//        return new BufferedDataTable[]{outTable1};
+// 
+//    }
+
     
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void reset() {
-        // TODO: generated method stub
+//    protected DataTableSpec[] configure(final DataTableSpec[] inSpecs)
+//            throws InvalidSettingsException {
+//    	
+//    		DataTableSpec outSpecTable1 = new DataTableSpec(
+//    										new DataColumnSpec[]{
+//    										new DataColumnSpecCreator(OUT_COL1_TABLE1, FileCell.TYPE).createSpec()});
+//    		
+//    		if(m_VCFCOLUMN.getIntValue()>inSpecs[0].getNumColumns()){
+//    			throw new InvalidSettingsException("Selected column "+m_VCFCOLUMN.getIntValue()+" is not available. inData has only "+inSpecs[0].getNumColumns()+" columns!");
+//    		}
+//    		
+//    		
+//    		
+//        return new DataTableSpec[]{outSpecTable1};
+//    }
+
+    protected String getCommandWalker() {
+    	return "SelectVariants";
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected DataTableSpec[] configure(final DataTableSpec[] inSpecs)
-            throws InvalidSettingsException {
-    	
-    		DataTableSpec outSpecTable1 = new DataTableSpec(
-    										new DataColumnSpec[]{
-    										new DataColumnSpecCreator(OUT_COL1_TABLE1, FileCell.TYPE).createSpec()});
-    		
-    		if(m_VCFCOLUMN.getIntValue()>inSpecs[0].getNumColumns()){
-    			throw new InvalidSettingsException("Selected column "+m_VCFCOLUMN.getIntValue()+" is not available. inData has only "+inSpecs[0].getNumColumns()+" columns!");
-    		}
-    		
-    		
-    		
-        return new DataTableSpec[]{outSpecTable1};
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void saveSettingsTo(final NodeSettingsWO settings) {
-   	 m_GATK.saveSettingsTo(settings);
-   	 m_REF_GENOME.saveSettingsTo(settings);
+    protected void saveExtraSettingsTo(final NodeSettingsWO settings) {
    	 m_VCFCOLUMN.saveSettingsTo(settings);
    	 m_FILTERSTRING.saveSettingsTo(settings);
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void loadValidatedSettingsFrom(final NodeSettingsRO settings)
+    protected void loadExtraValidatedSettingsFrom(final NodeSettingsRO settings)
             throws InvalidSettingsException {
-      	 m_GATK.loadSettingsFrom(settings);
-       	 m_REF_GENOME.loadSettingsFrom(settings);
        	 m_VCFCOLUMN.loadSettingsFrom(settings);
        	 m_FILTERSTRING.loadSettingsFrom(settings);
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void validateSettings(final NodeSettingsRO settings)
+    protected void validateExtraSettings(final NodeSettingsRO settings)
             throws InvalidSettingsException {
-      	 m_GATK.validateSettings(settings);
-       	 m_REF_GENOME.validateSettings(settings);
        	 m_VCFCOLUMN.validateSettings(settings);
        	 m_FILTERSTRING.validateSettings(settings);
-    }
-    
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void loadInternals(final File internDir,
-            final ExecutionMonitor exec) throws IOException,
-            CanceledExecutionException {
-        // TODO: generated method stub
-    }
-    
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void saveInternals(final File internDir,
-            final ExecutionMonitor exec) throws IOException,
-            CanceledExecutionException {
-        // TODO: generated method stub
     }
 
     protected SettingsModelString getFILTERSTRINGModel(){
