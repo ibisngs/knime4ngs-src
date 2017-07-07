393ec317eff01c533e110b0ce3d2ff919b343e2e
hastreiter <hastreiter@helmholtz-muenchen.de>
2017-07-07 12:11:26 +0200

    Remove unused nodes

--- de.helmholtz_muenchen.ibis.knimenodes/src/de/helmholtz_muenchen/ibis/ngs/featureCounts/FeatureCountsNodeModel.java
+++ de.helmholtz_muenchen.ibis.knimenodes/src/de/helmholtz_muenchen/ibis/ngs/featureCounts/FeatureCountsNodeModel.java
@@ -21,10 +21,8 @@ package de.helmholtz_muenchen.ibis.ngs.featureCounts;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
